703ca8f5fc6e43432ae5beb3d24f957878490bcc
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-09 09:11:12 +0100

    ft(create-artilces): users can create articles
    - users can apply basic formatting to text
    
    [Deliver 159987629]

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -8,6 +8,18 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import ProtectedRoute from './containers/ProtectedRoute';
 import Signin from './containers/SignIn/SignIn';
+import NavBar from './components/NavBar';
+import TextEditor from './containers/CreateArticle/TextEditor';
+
+const Login = () => (
+  <div><h1>Login page</h1></div>
+);
+
+const Sample = () => (
+  <div className="landing-page">
+    <h1>This is just a sample page for routes</h1>
+  </div>
+)
 
 const AppRoutes = () => (
   <Router>
@@ -18,6 +30,12 @@ const AppRoutes = () => (
       <ProtectedRoute path='/article' component={ArticlePage} />
       <ProtectedRoute path='/profile/user' component={ProfilePage} />
       <Route exact path='/signin' component={Signin} />
+      <ProtectedRoute path='/article' component={ ArticlePage }/>
+      <ProtectedRoute path='/profile/user' component={ ProfilePage }/>
+      <NavBar />
+      <Route exact path='/' component={ Homepage }/>
+      <Route exact path='/sample' component={ Sample }/>
+      <Route exact path='/create-article' component={ TextEditor }/>
       <Footer />
     </Fragment>
   </Router>
