f04155c86fe4374589d380c90541827bf2859bde
lauragift21 <laurandidi21@gmail.com>
2018-10-11 12:29:59 +0100

    feat(article): View single article page
    - displays a sing article
    - display related tags
    - write tests for feature
    
    [Delivers #161124006]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import auth from './auth';
 import signinuser from './signin';
 import signup from './signup';
 import comments from './comments';
+import article from './article';
 
 const rootReducer = combineReducers({
   auth,
   signinuser,
   signup,
   comments,
+  article
 });
 
 export default rootReducer;
--- src/store/initialState.js
+++ src/store/initialState.js
@@ -3,26 +3,33 @@ const initialState = {
     user: {},
     signin: {
       loading: false,
-      error: ''
+      error: ""
     },
     signup: {
       loading: false,
-      error: ''
+      error: ""
     },
     isAuthenticated: false
   },
   comments: {
     articleComments: {
       data: [],
-      error: '',
+      error: "",
       loading: false
     },
     newComment: {
       data: {},
-      error: '',
-      loading: false,
+      error: "",
+      loading: false
     }
+  },
+  article: {
+    article: {
+      author: {},
+      tags: [{}]
+    },
+    loading: false,
+    error: ""
   }
 };
-
 export default initialState;
