789b09be03bce9498a4ba21b5ed1ccb7fbea8447
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-12 12:59:52 +0100

    ft(create-articles): add validation to article creation
    - user cannot create article without title
    - user cannot create article without body
    
    [Delivers 159987629]

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,50 +1,29 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import React, { Fragment } from "react";
 // components
-import ProfilePage from './containers/ProfilePage';
-import ArticlePage from './containers/ArticlePage';
-import Homepage from './containers/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import ProtectedRoute from './containers/ProtectedRoute';
-import Signin from './containers/SignIn/SignIn';
-import NavBar from './components/NavBar';
-import CreateArticlePage from './containers/CreateArticle/CreateArticlePage';
-
-const Login = () => (
-  <div><h1>Login page</h1></div>
-);
-// import ProfilePage from "./containers/ProfilePage";
-// import ArticlePage from "./containers/ArticlePage";
-// import Homepage from "./containers/Home/Home";
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
-// import ProtectedRoute from "./containers/ProtectedRoute";
-// import NavBar from './components/NavBar';
-
-const Sample = () => (
-  <div className="landing-page">
-    <h1>This is just a sample page for routes</h1>
-  </div>
-);
+import ProfilePage from "./containers/ProfilePage";
+import ArticlePage from "./containers/ArticlePage";
+import Homepage from "./containers/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProtectedRoute from "./containers/ProtectedRoute";
+import Signin from "./containers/SignIn/SignIn";
+import CreateArticlePage from "./containers/CreateArticle/CreateArticlePage";
 
 const AppRoutes = () => (
   <Router>
     <Fragment>
       <Header />
-      <Route exact path='/' component={Homepage} />
-      {/* protected routes can be rendered using the ProtectedRoutes component */}
-      <ProtectedRoute path='/article' component={ArticlePage} />
-      <ProtectedRoute path='/profile/user' component={ProfilePage} />
-      <Route exact path='/signin' component={Signin} />
-      <ProtectedRoute path='/article' component={ ArticlePage }/>
-      <ProtectedRoute path='/profile/user' component={ ProfilePage }/>
-      <NavBar />
-      <Route exact path='/' component={ Homepage }/>
-      <Route exact path='/sample' component={ Sample }/>
-      <Route exact path='/create-article' component={ CreateArticlePage }/>
       <Route exact path="/" component={Homepage} />
-      <Route path="/login" component={Login} />
+      <Route exact path="/signin" component={Signin} />
+      {/* protected routes can be rendered using the ProtectedRoutes component */}
+      <ProtectedRoute path="/article" component={ArticlePage} />
+      <ProtectedRoute path="/profile/user" component={ProfilePage} />
+      <ProtectedRoute
+        exact
+        path="/create-article"
+        component={CreateArticlePage}
+      />
       <Footer />
     </Fragment>
   </Router>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,6 @@ import signinuser from './signin';
 import signup from './signup';
 import article from "./article";
 
-// initial dummy state setup
-const app = (state = { intro: "welcome to ah" }) => state;
-
 const rootReducer = combineReducers({
   auth,
   signinuser,
