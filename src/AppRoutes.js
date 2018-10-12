a2671957db505eb9c848d66b13c5dc700e529e9c
Godswill Onuoha <zezzy.godswill@gmail.com>
2018-10-12 11:25:00 +0100

    bg(frontend): fix header error and add 404 page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Fragment } from 'react'
 // components
 import ProfilePage from './containers/ProfilePage';
@@ -8,16 +8,22 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import ProtectedRoute from './containers/ProtectedRoute';
 import Signin from './containers/SignIn/SignIn';
+import NotFound from './containers/NotFound/NotFound';
 
 const AppRoutes = () => (
   <Router>
     <Fragment>
       <Header />
-      <Route exact path='/' component={Homepage} />
-      {/* protected routes can be rendered using the ProtectedRoutes component */}
-      <ProtectedRoute path='/article' component={ArticlePage} />
-      <ProtectedRoute path='/profile/user' component={ProfilePage} />
-      <Route exact path='/signin' component={Signin} />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        {/* protected routes can be rendered using the ProtectedRoutes component */}
+        <ProtectedRoute path='/article' component={ArticlePage} />
+        <ProtectedRoute path='/profile/user' component={ProfilePage} />
+        <Route exact path='/signin' component={Signin} />
+
+        {/* will alsways render when no other path is matched */}
+        <Route component={NotFound} />
+      </Switch>
       <Footer />
     </Fragment>
   </Router>
